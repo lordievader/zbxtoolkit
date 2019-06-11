660de0cf5c60978869eb5376e7270fc0b5c41b5c
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-06-11 19:40:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zbxtoolkit',
-    version='0.2.0',
+    version='0.2.1',
     description='Functions for managing Zabbix via the API.',
     author='Olivier van der Toorn',
     author_email='oliviervdtoorn@gmail.com',
